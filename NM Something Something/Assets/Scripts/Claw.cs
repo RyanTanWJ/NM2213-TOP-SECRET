e03ee82047b305ea67d5e11d4906cb54a0360646
RyanTWJ <ghost_heartbeat@yahoo.com.sg>
2018-10-20 18:45:44 +0800

    Clean up project viewer and created crab claw

---
+++ NM Something Something/Assets/Scripts/Claw.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Claw : MonoBehaviour {
+
+  private BoardManager.Direction clawDirection;
+
+  private float speed = 250.0f;
+
+  public bool ShouldMove = false;
+
+  // Use this for initialization
+  void Start()
+  {
+    clawDirection = BoardManager.Direction.RIGHT;
+  }
+
+  public void SetBoulderDirection(BoardManager.Direction direction)
+  {
+    clawDirection = direction;
+  }
+
+  public BoardManager.Direction GetDirection()
+  {
+    return clawDirection;
+  }
+
+  public float Speed
+  {
+    get { return speed; }
+  }
+}
