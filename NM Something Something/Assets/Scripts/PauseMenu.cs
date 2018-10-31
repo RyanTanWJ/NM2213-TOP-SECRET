f20b593678138bc53a6aa970248a1e18651d5dfa
RyanTWJ <ghost_heartbeat@yahoo.com.sg>
2018-10-31 13:39:30 +0800

    Pause Menu

---
+++ NM Something Something/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+  public static bool GameIsPaused = false;
+
+  [SerializeField]
+  private GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+    {
+      if (GameIsPaused)
+      {
+        Resume();
+      }
+      else
+      {
+        Pause();
+      }
+    }
+	}
+
+  private void Resume()
+  {
+    GameIsPaused = false;
+    pauseMenuUI.SetActive(GameIsPaused);
+    Time.timeScale = 1.0f;
+  }
+
+  private void Pause()
+  {
+    GameIsPaused = true;
+    pauseMenuUI.SetActive(GameIsPaused);
+    Time.timeScale = 0;
+  }
+}
