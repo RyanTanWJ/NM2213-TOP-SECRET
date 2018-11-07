4f0e2e32d39845fcc39cb313c9cbd73f9c5026a3
RyanTWJ <ghost_heartbeat@yahoo.com.sg>
2018-11-07 15:43:04 +0800

    WILHELM SCREAM

---
+++ NM Something Something/Assets/Scripts/StopBGM.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopBGM : MonoBehaviour {
+
+  [SerializeField]
+  private AudioSource BGM;
+
+  private void OnEnable()
+  {
+    Player.GameOverEvent += Stop;
+  }
+
+  private void OnDisable()
+  {
+    Player.GameOverEvent -= Stop;
+  }
+
+  private void Stop()
+  {
+    BGM.Stop();
+  }
+}
