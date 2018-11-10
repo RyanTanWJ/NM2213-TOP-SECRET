679c5a33e02c17247dd70f577c47a6f2102ee764
RyanTWJ <ghost_heartbeat@yahoo.com.sg>
2018-11-10 16:00:33 +0800

    Button Clicks

--- NM Something Something/Assets/Scripts/GoBack.cs
+++ NM Something Something/Assets/Scripts/GoBack.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoBack : MonoBehaviour {
 
   public delegate void GoBackDelegate();
   public static event GoBackDelegate GoBackEvent;
-	
-	// Update is called once per frame
-	void Update () {
+
+  [SerializeField]
+  private Button ReturnButton;
+
+  private void Start()
+  {
+    ReturnButton.onClick.AddListener(goBack);
+  }
+
+  // Update is called once per frame
+  void Update () {
     if (Input.GetKeyDown(KeyCode.Escape))
     {
-      GoBackEvent();
+      goBack();
     }
 	}
+
+  private void goBack()
+  {
+    GoBackEvent();
+  }
 }
